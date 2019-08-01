9779ec908e03979428c5cb53b1a906928fd6cc9a
Luke Becker <beckerluke1@gmail.com>
2019-08-01 17:38:46 -0500

    Added remove event listener to remove boxes by color. Added functions to remove boxes by color and keep track of count after removal. Took out initial js-container append so that no blocks pop up when doc loads to get accurate count of boxes

--- client.js
+++ client.js
@@ -13,37 +13,63 @@ function init() {
     $('.js-btn-blue').on('click', addBlueBox);
     $('.js-btn-red').on('click', addRedBox);
     $('.js-btn-yellow').on('click', addYellowBox);
+    
+    $('.js-container').on('click', '.green', removeGreenBox);
+    $('.js-container').on('click', '.red', removeRedBox);
+    $('.js-container').on('click', '.blue', removeBlueBox);
+    $('.js-container').on('click', '.yellow', removeYellowBox);
    
-    $('.js-container').append(`<div class="box green"></div>`)
-        .append(`<div class="box blue"></div>`)
-        .append(`<div class="box red"</div>`)
-        .append(`<div class="box yellow"</div>`);
+    // $('.js-container').append(`<div class="box green"></div>`)
+    //     .append(`<div class="box blue"></div>`)
+    //     .append(`<div class="box red"</div>`)
+    //     .append(`<div class="box yellow"</div>`);
 
-}
+}//end init function 
 
 function addGreenBox() {
-    let numOfGreenBoxes = 0; 
     numOfGreenBoxes++; 
     $('.js-greenBlock-counter').text('Green Box Count: ' + numOfGreenBoxes); 
     $('.js-container').append(`<div class="box green"></div>`);   
-}
+}//end addGreenBox function 
 
 function addBlueBox() {
     numOfBlueBoxes++; 
     $('.js-blueBlock-counter').text('Blue Box Count: ' + numOfBlueBoxes); 
     $('.js-container').append(`<div class="box blue"></div>`); 
-}
+}//end addBlueBox function
 
-function addRedBox() {
+function addRedBox() { 
     numOfRedBoxes++;
     $('.js-redBlock-counter').text('Red Box Count: ' + numOfRedBoxes); 
     $('.js-container').append(`<div class="box red"></div>`); 
-}
+}//end addRedBox function
 
 function addYellowBox() {
     numOfYellowBoxes++;
     $('.js-yellowBlock-counter').text('Yellow Box Count: ' + numOfYellowBoxes);  
     $('.js-container').append(`<div class="box yellow"></div>`); 
+} //end addYellowBox function 
 
+function removeGreenBox() { 
+    numOfGreenBoxes--; 
+    $(this).remove();  
+    $('.js-greenBlock-counter').text('Green Box Count: ' + numOfGreenBoxes);
+}
 
+function removeBlueBox() {
+    numOfBlueBoxes--; 
+    $(this).remove();  
+    $('.js-blueBlock-counter').text('Blue Box Count: ' + numOfBlueBoxes);
+}
+
+function removeRedBox() {
+    numOfRedBoxes--; 
+    $(this).remove();  
+    $('.js-redBlock-counter').text('Red Box Count: ' + numOfRedBoxes);
+}
+
+function removeYellowBox() {
+    numOfYellowBoxes--; 
+    $(this).remove();  
+    $('.js-yellowBlock-counter').text('Yellow Box Count: ' + numOfYellowBoxes);
 }
