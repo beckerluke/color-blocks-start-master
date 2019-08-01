8c61c5a333c83ddf8f89fe59586dade47dbe4db6
Luke Becker <beckerluke1@gmail.com>
2019-07-31 22:01:12 -0500

    Initial Commit

---
+++ client.js
@@ -0,0 +1,8 @@
+$(document).ready(init);
+
+function init() {
+    $('.js-container').append(`<div class="box green"></div>`)
+        .append(`<div class="box blue"></div>`)
+        .append(`<div class="box red"</div>`)
+        .append(`<div class="box yellow"</div>`);
+}
