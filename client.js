cb70d2e5d6961f80bd46171639fa4fd18a436869
Luke Becker <beckerluke1@gmail.com>
2019-08-01 16:48:04 -0500

    Added 4 buttons and 4 functions to add blocks of each color when clicked, and added counters to keep track of the carrying amount of blocks added

--- client.js
+++ client.js
@@ -1,8 +1,49 @@
+let numOfGreenBoxes = 0; 
+let numOfBlueBoxes = 0; 
+let numOfRedBoxes = 0; 
+let numOfYellowBoxes = 0; 
+
 $(document).ready(init);
 
 function init() {
+    
+    const colorBlocks= ['green', 'blue', 'red', 'yellow']; 
+
+    $('.js-btn-green').on('click', addGreenBox);
+    $('.js-btn-blue').on('click', addBlueBox);
+    $('.js-btn-red').on('click', addRedBox);
+    $('.js-btn-yellow').on('click', addYellowBox);
+   
     $('.js-container').append(`<div class="box green"></div>`)
         .append(`<div class="box blue"></div>`)
         .append(`<div class="box red"</div>`)
         .append(`<div class="box yellow"</div>`);
+
+}
+
+function addGreenBox() {
+    let numOfGreenBoxes = 0; 
+    numOfGreenBoxes++; 
+    $('.js-greenBlock-counter').text('Green Box Count: ' + numOfGreenBoxes); 
+    $('.js-container').append(`<div class="box green"></div>`);   
+}
+
+function addBlueBox() {
+    numOfBlueBoxes++; 
+    $('.js-blueBlock-counter').text('Blue Box Count: ' + numOfBlueBoxes); 
+    $('.js-container').append(`<div class="box blue"></div>`); 
+}
+
+function addRedBox() {
+    numOfRedBoxes++;
+    $('.js-redBlock-counter').text('Red Box Count: ' + numOfRedBoxes); 
+    $('.js-container').append(`<div class="box red"></div>`); 
+}
+
+function addYellowBox() {
+    numOfYellowBoxes++;
+    $('.js-yellowBlock-counter').text('Yellow Box Count: ' + numOfYellowBoxes);  
+    $('.js-container').append(`<div class="box yellow"></div>`); 
+
+
 }
